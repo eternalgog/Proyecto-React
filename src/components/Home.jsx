778d07c66b66583f29e704d34e529391e5a64012
Eternalgog <irving.gonzalezgr@hotmail.com>
2024-06-10 11:38:10 -0600

    Actualizacion

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ export const Home = () => {
     <div className="w3-container" style={{ textAlign: 'center' }}>
       <h1 className="w3-text-teal" style={{ fontSize: '50px' }}>¡Bienvenido a PsicoVida!</h1>
       <p className="w3-text-grey w3-padding" style={{ fontSize: '20px', marginTop: '20px', maxWidth: '600px', margin: 'auto', boxShadow: '0 3px 10px 0 rgba(0, 0, 0, 0.3)', borderRadius: '8px' }}>
-        PsicoVida es una plataforma diseñada para gestionar eficientemente las citas, asignar psicólogos a pacientes y mantener un control exhaustivo de los datos. ¡Explora todas las funcionalidades que ofrecemos para mejorar tu experiencia en la gestión de consultas psicológicas!
+        PsicoVida es una plataforma diseñada para gestionar eficientemente las citas en la clínica, dar de altas psicólogos y pacientes para tener un control de los datos. ¡Para poder hacer citas, es necesario tener psicólogos y pacientes!
       </p>
     </div>
   );
