3454ff453202ac86f2de17b639d35644b4bf9465
Eternalgog <irving.gonzalezgr@hotmail.com>
2024-06-15 10:41:17 -0600

    Logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export const NavBar = () => {
   return (
     <div className="w3-bar w3-light-grey" style={{ textAlign: 'center', overflow: 'hidden' }}>
-      <img src="https://scontent.fcme1-1.fna.fbcdn.net/v/t39.30808-6/347823877_2582390938591715_1036047327070216785_n.png?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Ckk0C0TLukkQ7kNvgHrqkn4&_nc_ht=scontent.fcme1-1.fna&oh=00_AYAGwDAdKpwhpfKqilw027CPDlOabJgymuw-gpIlrMarHw&oe=666C0315" alt="Logo" className="w3-image" style={{ maxWidth: '100px', margin: 'auto' }} />
+      <img src="src\images.png" alt="Logo" className="w3-image" style={{ maxWidth: '100px', margin: 'auto' }} />
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <NavLink to='/' className="w3-bar-item w3-button w3-hover-black" style={{ fontSize: '20px' }}>Home</NavLink>
         <NavLink to='/altadoctores' className="w3-bar-item w3-button w3-hover-black" style={{ fontSize: '20px' }}>Alta Psicólogos</NavLink>
